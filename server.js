2895174b79e0813c440d4d83775b1e1665491b8e
pavan <praveenpj000@gmail.com>
2023-12-19 15:20:35 +0530

    initial author Routes

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-// if(process.env.NODE_ENV !== 'production') {
-//     require('dotenv').parse()
-// }
+require('dotenv').config();
+
+if(process.env.DATABASE_URL!== 'production') {
+    require('dotenv').parse()
+ }
 const express = require('express');
+
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const uri = 'mongodb+srv://praveenpj000:pavan6747@cluster0.vfxowr4.mongodb.net/pavan?retryWrites=true&w=majority'
 
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bodyParser = require('body-parser');
 app.set('view engine','ejs');
 app.set('views',__dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 const mongoose = require('mongoose');
 app.use(express.json());
-mongoose.connect(uri,{
+mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -25,5 +30,6 @@ db.on('error', error => console.error(error));
 db.once('open',() => console.log('connected to Mongoose'));
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 
 app.listen(process.env.PORT || 3000);
